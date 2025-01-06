7ad17d9d18974bf3ca6e40787477f2e2ebf53f2d
nishankster <49107179+nishankster@users.noreply.github.com>
2025-01-05 21:16:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,1 @@
-// Initialize Parallax
-document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.querySelectorAll('.parallax-window');
-    elements.forEach(function(el) {
-        new Parallax(el);
-    });
-});
+

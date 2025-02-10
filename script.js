2c0dd0a261e0d9053517578f56cebc5a65a5f1c1
nishankster <49107179+nishankster@users.noreply.github.com>
2025-02-09 20:00:19 -0500

    first commit for nishankster

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from("header", { opacity: 0, y: -50, duration: 1 });
+gsap.from(".hero h2", { opacity: 0, y: -50, duration: 1, delay: 0.5 });
+gsap.from(".hero p", { opacity: 0, y: -50, duration: 1, delay: 1 });
+gsap.from(".hero .btn", { opacity: 0, y: -50, duration: 1, delay: 1.5 });
